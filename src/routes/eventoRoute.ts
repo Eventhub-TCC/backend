e20fb881883daedb208a9f8ce25b330e91f62289
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2025-04-16 14:00:31 -0300

    feat(backend): editar evento -atualizadas rotas, controllers e dao para editar evento

--- src/routes/eventoRoute.ts
+++ src/routes/eventoRoute.ts
@@ -9,7 +9,9 @@ const eventoController = new EventoController()
 route.post('/:idUsuario/events',upload.single("file"), eventoController.cadastrarEvento);
 route.get('/events/:emailUsu', eventoController.listarEventos);
 route.get('/:idUsuario/events/:idEvento', eventoController.buscarEventoporId);
-route.put('/:idUsuario/events/:idEvento', eventoController.editarEvento);
+route.put('/events/:idEvento', async (req, res) => {
+    await eventoController.editarEvento(req, res);
+});
 route.delete('/:idUsuario/events/:idEvento', eventoController.deletarEvento);
 
 export default route;
