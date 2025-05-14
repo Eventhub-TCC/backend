799361ee720abd8c6b003ae3b6c9d814e7274b5d
Matheus Alves de Paula <matheus.paula11@fatec.sp.gov.br>
2025-05-14 16:43:46 -0300

    feat(servico): cria servico e tipo servico - faz cadastro de serviços - cria dao, controller, model, route do servico e tipo servico - adiciona dados iniciais ao tipo servico

---
+++ src/routes/servicoRoute.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import ServicoController from '../controllers/ServicoController';
+import upload from "../config/multer";
+import { validarTokenAutenticacao } from "../middlewares/validarToken";
+
+const route = express.Router()
+
+const servicoController = new ServicoController()
+
+route.post('/services', validarTokenAutenticacao, upload.array('files',6) ,servicoController.cadastrarServico)
+
+export default route;
