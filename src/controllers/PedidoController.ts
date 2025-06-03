67c3f7dfec658943a97a45d0c7fb2ac8f0701b54
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2025-06-03 11:24:12 -0300

    feat(backend): adiciona pedido e item de pedido - Cria Controllers, DAO, e Models para Pedido e ItemPedido - Implementa métodos para criação e listagem de pedidos

---
+++ src/controllers/PedidoController.ts
@@ -0,0 +1,51 @@
+import PedidoDAO from '../dao/PedidoDao';
+
+export default class PedidoController {
+    private pedidoDao = new PedidoDAO();
+
+    public finalizarPedido = async (req: any, res: any) => {
+        try {
+            const { codigoUsu, idEvento, itens } = req.body;
+
+            if (!Array.isArray(itens) || itens.length === 0) {
+                return res.status(400).json({ mensagem: "Nenhum item informado" });
+            }
+
+            const novoPedido = await this.pedidoDao.finalizarPedido(codigoUsu, idEvento, itens);
+            res.status(201).json(novoPedido);
+        } catch (error) {
+            console.error("Erro ao criar pedido:", error);
+            res.status(500).json({ mensagem: "Erro interno ao criar pedido" });
+        }
+    }
+
+    public listarPedidos = async (req: any, res: any) => {
+        try {
+            const codigoUsu = req.user!.codigoUsu;
+            const pedidos = await this.pedidoDao.listarPedidos(codigoUsu);
+            if (pedidos.length === 0) {
+                return res.status(404).json({ mensagem: "Nenhum pedido encontrado" });
+            }
+            res.status(200).json(pedidos);
+        } catch (error) {
+            console.error("Erro ao listar pedidos:", error);
+            res.status(500).json({ mensagem: "Erro interno ao listar pedidos" });
+        }
+    }
+
+    public cancelarPedido = async (req: any, res: any) => {
+        const { idPedido } = req.body;
+
+        try {
+            const pedido = await this.pedidoDao.cancelarPedido(idPedido);
+            if (!pedido) {
+                return res.status(404).json({ mensagem: "Pedido não encontrado" });
+            }
+            res.status(200).json({ mensagem: "Pedido cancelado com sucesso" });
+        } catch (error) {
+            console.error("Erro ao cancelar pedido:", error);
+            res.status(500).json({ mensagem: "Erro interno ao cancelar pedido" });
+        }
+    }
+
+}
---
+++ src/dao/PedidoDao.ts
@@ -0,0 +1,99 @@
+import { Transaction } from "sequelize";
+import { sequelize } from "../config/database";
+import Pedido from "../models/Pedido";
+import ItemPedido from "../models/ItemPedido";
+import Evento from "../models/Evento";
+
+export default class PedidoDao {
+    public finalizarPedido = async (
+        codigoUsu: string,
+        idEvento: number,
+        itens: {
+            idServico: string;
+            nomeItem: string;
+            valorUnitario: number;
+            quantidade: number;
+            instrucoes?: string;
+        }[]
+    ) => {
+        const t: Transaction = await sequelize.transaction(); //transação para caso algo de errado usar o rollback
+
+        try {
+            const pedido = await Pedido.create({ //gravando o pedido no banco sem os itens por enquanto (e sem total)
+                codigoUsu: codigoUsu,
+                idEvento,
+                status: 'pendente',
+                dataCriacao: new Date(),
+            }, { transaction: t });
+
+            let valorTotal = 0;                 //partindo pro calculo do valor total do pedido
+            for (const item of itens) {         //iterando sobre cada item do pedido
+                const subtotal = item.valorUnitario * item.quantidade;  //calculando o total de cada item a partir da quantidade e preço unitario
+                valorTotal += subtotal; //incrementando o valor total do pedido
+
+                await ItemPedido.create({
+                    idPedido: pedido.idPedido,
+                    idServico: item.idServico,
+                    nomeItem: item.nomeItem,
+                    valorUnitario: item.valorUnitario,
+                    quantidade: item.quantidade,
+                    instrucoes: item.instrucoes,
+                    valorTotal: subtotal,
+                }, { transaction: t });
+            }
+
+
+            pedido.valorTotal = valorTotal;
+            await pedido.save({ transaction: t });
+            await t.commit();
+
+            return pedido;
+        } catch (error) {
+            await t.rollback();
+            throw error;
+        }
+    }
+
+    public listarPedidos = async (codigoUsu: string) => {
+        const pedidos = await Pedido.findAll({
+            where: { codigoUsu },
+            include: [{
+                model: ItemPedido,
+                as: 'itens',
+                required: false
+            }],
+            order: [['dataPedido', 'DESC']]
+        });
+
+        return pedidos;
+    }
+
+    public cancelarPedido = async (idPedido: string) => {
+    const pedido = await Pedido.findByPk(idPedido, {
+        include: [{ model: Evento, as: 'Evento' }], // Traz o evento junto com inner join
+    });
+
+    if (!pedido) {
+        throw new Error('Pedido não encontrado');
+    }
+
+    const evento = (pedido as any).Evento;
+
+    if (!evento || !evento.dataEvento) {
+        throw new Error('Evento associado não encontrado ou sem data definida');
+    }
+
+    const agora = new Date();
+    const dataEvento = new Date(evento.dataEvento);
+    const diffHoras = (dataEvento.getTime() - agora.getTime()) / (1000 * 60 * 60); //verifica a diferença em horas
+
+    if (diffHoras <= 48) {
+        throw new Error('Não é possível cancelar pedidos para eventos que ocorrerão em menos de 48 horas'); 
+    }
+
+    pedido.status = 'cancelado';
+    await pedido.save();
+    return pedido;
+    };
+
+}
